5239756c58882cefd0fab64d81d7e763ffb65899
Ergashev Akbar <ergashev.akb@gmail.com>
2020-02-28 01:12:46 +0500

    First draft of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import enTranslations from "@shopify/polaris/locales/en.json";
+import {
+    AppProvider,
+    Page,
+    Spinner,
+    DisplayText,
+    Card
+} from "@shopify/polaris";
+import CitySelector from "./components/CitySelector";
+
+const cities = ["Dushanbe", "Moscov", "New-York"];
+
+const currentLocation = cities[0];
+
+const openWeatherApiKey = "2433e639987bfba3d32b434e917d92b4";
 
 function App(props) {
-    return <div></div>;
+    const [weather, setWeather] = useState();
+    const [city, setCity] = useState(currentLocation);
+
+    const fetchWeatherData = () => {
+        const apiEndpointURL =
+            "http://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric" +
+            "&appid=" +
+            openWeatherApiKey;
+
+        fetch(apiEndpointURL)
+            .then(res => res.json())
+            .then(data => setWeather(data.main));
+    };
+
+    useEffect(() => {
+        fetchWeatherData();
+    }, [city]);
+
+    const onCitySelected = selectedCity => {
+        setCity(selectedCity);
+    };
+
+    return (
+        <AppProvider i18n={enTranslations}>
+            <Page title="Weather">
+                <Card sectioned>
+                    <div className="centered">
+                        {weather ? (
+                            <>
+                                <DisplayText size="large">{city}</DisplayText>
+                                <DisplayText size="extraLarge">
+                                    {weather.temp} °C
+                                </DisplayText>
+                            </>
+                        ) : (
+                            <Spinner size="large" color="teal" />
+                        )}
+                    </div>
+
+                    <CitySelector
+                        cities={cities}
+                        onCitySelected={onCitySelected}
+                    />
+                </Card>
+            </Page>
+        </AppProvider>
+    );
 }
 
 export default App;
